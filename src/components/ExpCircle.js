79be275e7253d6a1974476cadf43ff0f1604ff0a
Ali Iqbal <ali.j.iqbal24@gmail.com>
2025-06-21 18:58:43 -0600

    Adjust leveling system and progress calculations

--- src/components/ExpCircle.js
+++ src/components/ExpCircle.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import * as Progress from 'react-native-progress';
-
-export default function ExpCircle({ exp, totalExp = 20, size = 100 }) {
-  const progress = (exp % totalExp) / totalExp;
+import { getLevelInfo } from '../utils/levelUtils';
+export default function ExpCircle({ exp, size = 100 }) {
+  const { expThisLevel, expForNext } = getLevelInfo(exp);
+  const progress = expThisLevel / expForNext;
   return (
     <Progress.Circle
       size={size}
       progress={progress}
       showsText
-      formatText={() => `${exp % totalExp}/${totalExp}`}
+      formatText={() => `${expThisLevel}/${expForNext}`}
       thickness={8}
       color="#4CAF50"
       unfilledColor="#eee"

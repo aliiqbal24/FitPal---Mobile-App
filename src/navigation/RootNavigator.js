43c6a83f21d853b0f5c1b0fc90eec5b26e8ea853
Ali Iqbal <ali.j.iqbal24@gmail.com>
2025-06-24 20:48:35 -0600

    Add blank onboarding screens

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -4,6 +4,9 @@ import TabNavigator from './TabNavigator';
 import SettingsScreen from '../screens/SettingsScreen';
 import ActivityScreen from '../screens/ActivityScreen';
 import FriendsScreen from '../screens/FriendsScreen';
+import Onboarding1Screen from '../screens/Onboarding1Screen';
+import Onboarding2Screen from '../screens/Onboarding2Screen';
+import Onboarding3Screen from '../screens/Onboarding3Screen';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +17,9 @@ export default function RootNavigator() {
       <Stack.Screen name="Settings" component={SettingsScreen} />
       <Stack.Screen name="Activity" component={ActivityScreen} />
       <Stack.Screen name="Friends" component={FriendsScreen} />
+      <Stack.Screen name="Onboarding1" component={Onboarding1Screen} />
+      <Stack.Screen name="Onboarding2" component={Onboarding2Screen} />
+      <Stack.Screen name="Onboarding3" component={Onboarding3Screen} />
     </Stack.Navigator>
   );
 }
---
+++ src/screens/Onboarding1Screen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+
+export default function Onboarding1Screen() {
+  return <SafeAreaView style={styles.container} />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});

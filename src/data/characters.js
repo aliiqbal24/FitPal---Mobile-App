7f8af82976845e245a4a181dd4a0f724965e7667
aliiqbal24 <ali.j.iqbal24@gmail.com>
2025-06-26 22:13:16 -0600

    fix

--- src/data/characters.js
+++ src/data/characters.js
@@ -1,10 +1,6 @@
 export const CHARACTER_IMAGES = {
-  GiraffeF: require('../../assets/characters/GiraffeF.png'),
-  GiraffeM: require('../../assets/characters/GiraffeM.png'),
   GorillaF: require('../../assets/characters/GorillaF.png'),
   GorillaM: require('../../assets/characters/GorillaM.png'),
-  OwlF: require('../../assets/characters/OwlF.png'),
-  OwlM: require('../../assets/characters/OwlM.png'),
 };
 
 export const CHARACTER_OPTIONS = Object.keys(CHARACTER_IMAGES).map(key => ({

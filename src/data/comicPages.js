68b81f919007c5eaae9e6972710d8a65bc98df7b
Ali Iqbal <ali.j.iqbal24@gmail.com>
2025-06-28 18:43:54 -0600

    fix comic page image paths with proper casing

--- src/data/comicPages.js
+++ src/data/comicPages.js
@@ -1,6 +1,6 @@
 export const COMIC_IMAGES = [
-  require('../../assets/Comic/page1Door.png'),
-  require('../../assets/Comic/page2Gorilla.png'),
+  require('../../assets/Comic/Page1Door.png'),
+  require('../../assets/Comic/Page2Gorilla.png'),
   require('../../assets/Comic/Page3Gorilla.png'),
   require('../../assets/Comic/Page4Gorilla.png'),
   require('../../assets/Comic/Page5Gorilla.png'),

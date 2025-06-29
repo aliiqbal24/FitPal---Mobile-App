fa46bb998b3b4daeddfda8926cdd5e2e9c2e483c
Ali Iqbal <ali.j.iqbal24@gmail.com>
2025-06-28 23:16:49 -0600

    Add narration boxes to comic slides

---
+++ src/data/comicNarrations.js
@@ -0,0 +1,22 @@
+export const COMIC_NARRATIONS = [
+  {
+    text: 'Later that night…',
+    style: { top: 20, left: 20 },
+  },
+  {
+    text: 'Suddenly, a shadow appears',
+    style: { top: 20, right: 20 },
+  },
+  {
+    text: 'The gorilla approaches!',
+    style: { bottom: 20, left: 20 },
+  },
+  {
+    text: 'Our hero is shocked',
+    style: { top: 20, left: 20 },
+  },
+  {
+    text: 'What will happen next?',
+    style: { bottom: 20, right: 20 },
+  },
+];

662538040a762440d5cf0fef3cd200c38f963b21
aliiqbal24 <ali.j.iqbal24@gmail.com>
2025-06-30 22:09:10 -0600

    ali

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-const DefaultConfig = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.sourceExts.push('cjs');
 
 module.exports = defaultConfig;

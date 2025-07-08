cde83ae38657e8131d1abc166c11f61f423d9f21
Ali Iqbal <ali.j.iqbal24@gmail.com>
2025-07-07 21:44:16 -0600

    Remove placeholder character and duplicate existing image for gallery

--- src/data/characters.js
+++ src/data/characters.js
@@ -1,6 +1,9 @@
 export const CHARACTER_IMAGES = {
   GorillaF: require('../../assets/characters/GorillaF.png'),
+  GorillaF2: require('../../assets/characters/GorillaF2.png'),
+  GorillaF3: require('../../assets/characters/GorillaF3.png'),
   GorillaM: require('../../assets/characters/GorillaM.png'),
+  GorillaM2: require('../../assets/characters/GorillaM.png'),
 };
 
 export const CHARACTER_OPTIONS = Object.keys(CHARACTER_IMAGES).map(key => ({
